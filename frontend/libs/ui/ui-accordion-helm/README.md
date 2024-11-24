9d67c43564ffe567006056ec43e27a7c253dc4f8
Ferovac <85068658+Jure-Rajcic@users.noreply.github.com>
2024-11-24 19:51:17 +0100

    made sidebar component for client"

---
+++ frontend/libs/ui/ui-accordion-helm/README.md
@@ -0,0 +1,9 @@
+# ui-accordion-helm
+
+This library was generated with [Nx](https://nx.dev).
+
+
+## Running unit tests
+
+Run `nx test ui-accordion-helm` to execute the unit tests.
+
--- frontend/src/app/sidebar-component.ts
+++ frontend/src/app/sidebar-component.ts
@@ -1,7 +1,7 @@
 
 import { Component } from '@angular/core';
 import { provideIcons } from '@ng-icons/core';
-import { lucideCalendar, lucideBookOpenCheck, lucideEuro, lucideSettings2, lucidePlus, lucideMenu, lucideHouse} from '@ng-icons/lucide';
+import { lucideCalendar, lucideBookOpenCheck, lucideEuro, lucideSettings2, lucideLanguages, lucidePlus, lucideInfo, lucideReceiptEuro, lucideLogOut, lucideMenu, lucideHouse, lucideBrain, lucideMonitorCheck, lucideMoon, lucideCross } from '@ng-icons/lucide';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
@@ -25,12 +25,20 @@ import {
   BrnCollapsibleTriggerDirective,
 } from '@spartan-ng/ui-collapsible-brain';
 
+import {
+  HlmAccordionContentComponent,
+  HlmAccordionDirective,
+  HlmAccordionIconDirective,
+  HlmAccordionItemDirective,
+  HlmAccordionTriggerDirective,
+} from '@spartan-ng/ui-accordion-helm';
 
 
 @Component({
   selector: 'spartan-typography-preview',
   standalone: true,
-  providers: [provideIcons({ lucideMenu }), provideIcons({ lucideHouse }), provideIcons({ lucidePlus }), provideIcons({ lucideCalendar }), provideIcons({ lucideBookOpenCheck }), provideIcons({ lucideEuro }), provideIcons({ lucideSettings2 })],
+  providers: [provideIcons({ lucideMenu }), provideIcons({ lucideHouse }), provideIcons({ lucidePlus }), provideIcons({ lucideCalendar }), provideIcons({ lucideBookOpenCheck }), provideIcons({ lucideEuro }), provideIcons({ lucideSettings2 }), 
+    provideIcons({ lucideBrain }), provideIcons({ lucideMonitorCheck }), provideIcons({ lucideCross }), provideIcons({ lucideLanguages }), provideIcons({ lucideMoon }), provideIcons({ lucideLogOut }), provideIcons({ lucideReceiptEuro }), provideIcons({ lucideInfo })],
   imports: [
     BrnSheetTriggerDirective,
     BrnSheetContentDirective,
@@ -51,6 +59,12 @@ import {
     HlmIconComponent,
     BrnHoverCardModule, HlmHoverCardModule, HlmButtonDirective, HlmIconComponent, HlmAvatarModule,
     HlmAvatarImageDirective, HlmAvatarComponent, HlmAvatarFallbackDirective,
+    HlmAccordionDirective,
+    HlmAccordionItemDirective,
+    HlmAccordionTriggerDirective,
+    HlmAccordionContentComponent,
+    HlmAccordionIconDirective,
+    HlmIconComponent,
   ],
   templateUrl: './sidebar-component.html',
 
